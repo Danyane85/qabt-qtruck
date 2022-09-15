8eb7338451a37ad042afaa278fc29d5c1d2b122b
unknown <danyane.carmem@gmail.com>
2022-09-14 21:20:40 -0300

    Meu primeiro commit com git

---
+++ tests/cypress/e2e/login.cy.js
@@ -0,0 +1,89 @@
+import loginPage from '../support/pages/Login'
+import mapPage from '../support/pages/Map'
+
+
+
+describe('Login', () => {
+  it('deve logar com sucesso', () => {
+
+    const user = {
+      name: 'Maria',
+      instagran: '@maria',
+      password: 'maria123'
+    }
+
+    loginPage.go()
+    loginPage.form(user)
+    loginPage.submit()
+
+    mapPage.loggedUser(user.name)
+
+    
+  })
+  it('nao deve logar com senha invalida', () => {
+
+    const user = {
+      instagran: '@maria',
+      password: 'ma123'
+    }
+
+    loginPage.go()
+    loginPage.form(user)
+    loginPage.submit()
+
+    loginPage.modal.haveText('Credenciais inválidas, tente novamente!')
+    //cy.modalHaveText('Credenciais inválidas, tente novamente!')
+
+
+  })
+
+  it('nao deve logar com instagram inexistente', () => {
+    cy.visit('/')
+    cy.get('input[name=instagram]').type('@mte')
+    cy.get('input[name=password]').type('maria123')
+
+    cy.contains('button', 'Entrar').click()
+
+    cy.get('.swal2-html-container')
+      .should('be.visible')
+      .should('have.text', 'Credenciais inválidas, tente novamente!')
+
+  })
+
+  it('instagram deve ser obrigatório', () => {
+    const user = {
+      instagram: '',
+      password: 'maria123'
+    }
+
+    loginPage.go()
+    loginPage.form(user)
+    loginPage.submit()
+    loginPage.modal.haveText('Por favor, informe o seu código do Instagram!')
+    
+  })
+
+  it('senha deve ser obrigatória', () => {
+    const user = {
+      instagran: '@maria' 
+    }
+
+    loginPage.go()
+    loginPage.form(user)
+    loginPage.submit()
+    loginPage.modal.haveText('Por favor, informe a sua senha secreta!')
+   
+  })
+
+  it('todos os campos são obrigatórios', ()=> {
+    const user = {}
+      
+    loginPage.go()
+    loginPage.form(user)
+    loginPage.submit()
+    loginPage.modal.haveText('Por favor, informe suas credenciais!')
+   
+  })
+})
+
+
